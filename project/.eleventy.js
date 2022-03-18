0595aa2e066e8e8ac23fd3c52cad66632e9527f8
emilyxlin <swayxemily@gmail.com>
2022-03-18 10:11:32 -0400

    layouts step 1

--- project/.eleventy.js
+++ project/.eleventy.js
@@ -2,8 +2,12 @@ module.exports = function (eleventyConfig) {
     return {
       dir: {
         input: "src",
-        includes:  ""
+        includes:  "_includes",
         output: "_site",
       },
+      templateFormats: ['md', 'njk', 'html'],
+      markdownTemplateEngine: 'njk',
+      htmlTemplateEngine: 'njk',
+      dataTemplateEngine: 'njk',
     };
   };
