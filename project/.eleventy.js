38b02996ca3b6356576e2305a877bf4e30325266
emilyxlin <swayxemily@gmail.com>
2022-04-04 09:59:24 -0400

    hover intro

--- project/.eleventy.js
+++ project/.eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/style");
   eleventyConfig.addPassthroughCopy("./src/md");
   eleventyConfig.addPassthroughCopy("./src/media");
-  eleventyConfig.addPassthroughCopy("./src/style");
     return {
       dir: {
         input: "src",
