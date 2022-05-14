9f019aebb59fca33ba563c54f3c747892213fb1f
emilyxlin <90630627+emilyxlin@users.noreply.github.com>
2022-05-14 14:48:00 -0400

    fixing css

--- project/src/app.js
+++ project/src/app.js
@@ -1,9 +1,6 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('nav-links');
-    burger.addEventListener('click', ()=> {
-        nav.classList.toggle('nav-active');
-    })
-}
+const toggleButton = document.getElementsByClassName("toggle-button")[0];
+const navbarLinks = document.getElementsByClassName("navbar-links")[0];
 
-navSlide();
+toggleButton.addEventListener("click", () => {
+  navbarLinks.classList.toggle("active");
+});
