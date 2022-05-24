5a9574a5d10061eec579446669d7efa206129d50
emilyxlin <swayxemily@gmail.com>
2022-05-24 10:10:11 -0400

    toggles

--- project/src/js/app.js
+++ project/src/js/app.js
@@ -4,3 +4,14 @@ const navbarLinks = document.getElementsByClassName('navlinks')[0]
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
+
+selectors.mode.addEventListener("click", function () {
+    if (document.body.classList.contains("blue")) {
+      document.body.classList.add("green");
+      document.body.classList.remove("blue");
+    } else {
+      document.body.classList.add("blue");
+      document.body.classList.remove("green");
+    }
+  });
+  
