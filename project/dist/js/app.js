336d1a632dbf1bbce65956ccf28514d3b7278ce1
emilyxlin <swayxemily@gmail.com>
2022-05-25 10:11:20 -0400

    themes

--- project/dist/js/app.js
+++ project/dist/js/app.js
@@ -5,13 +5,29 @@ toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
 
-selectors.mode.addEventListener("click", function () {
-    if (document.body.classList.contains("blue")) {
-      document.body.classList.add("green");
-      document.body.classList.remove("blue");
-    } else {
-      document.body.classList.add("blue");
-      document.body.classList.remove("green");
+// light = blue
+// dark = green 
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'green');
     }
-  });
-  
+    else {
+        document.documentElement.setAttribute('data-theme', 'blue');
+    }    
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+// selectors.mode.addEventListener("click", function () {
+//       if (document.body.classList.contains("blue")) {
+//       document.body.classList.add("green");
+//       document.body.classList.remove("blue");
+//        } else {
+//       document.body.classList.add("blue");
+//       document.body.classList.remove("green");
+//        }
+// });
